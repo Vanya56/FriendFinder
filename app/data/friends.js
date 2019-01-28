f103443c017de537e60315f70a9af606c4ea1465
Vanya Levchenko <vanyalevchenko@Vanyas-MacBook-Pro.local>
2019-01-27 22:54:00 -0700

    routing and data finnished

---
+++ app/data/friends.js
@@ -0,0 +1,67 @@
+var friends = [{
+    name: "Liam",
+    photo: "http://www.gstatic.com/tv/thumb/persons/1236/1236_v9_bb.jpg",
+    scores: [
+        "1",
+        "3",
+        "5",
+        "2",
+        "2",
+        "5",
+        "1",
+        "1",
+        "4",
+        "2"
+    ]
+},
+{
+    name: "Bill Murray",
+    photo: "https://vignette.wikia.nocookie.net/ghostbusters/images/1/11/Bill_Murray.jpeg/revision/latest?cb=20081013140955",
+    scores: [
+        "2",
+        "5",
+        "4",
+        "1",
+        "1",
+        "4",
+        "3",
+        "3",
+        "5",
+        "1"
+    ]
+},
+{
+    name: "Shean Connery",
+    photo: "https://www.lunigal.com/images/stars/sean_connery-1676.jpg",
+    scores: [
+        "1",
+        "1",
+        "3",
+        "5",
+        "4",
+        "2",
+        "2",
+        "3",
+        "1",
+        "1"
+    ]
+},
+{
+    name: "Jennifer Aniston",
+    photo: "https://i2.wp.com/celebrityinside.com/wp-content/uploads/2014/05/Jennifer-Aniston-Favorite-Things-Biography-Net-worth-Facts.jpg?fit=1018%2C1338&ssl=1",
+    scores: [
+        "5",
+        "1",
+        "3",
+        "3",
+        "5",
+        "2",
+        "2",
+        "4",
+        "2",
+        "1"
+    ]
+},
+]
+
+module.exports = friends;
